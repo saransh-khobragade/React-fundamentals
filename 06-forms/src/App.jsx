765971ac8e52e7318f22e76c8c3840a0dc7d2089
saransh-khobragade <saransh98@gmail.com>
2025-06-24 20:18:41 +0530

    forms added

--- 06-forms/src/App.jsx
+++ 06-forms/src/App.jsx
@@ -1,30 +1,46 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 function App() {
-  const alertPlease= (x) => alert(x)
-  const passRef = useRef()
+  const alertPlease = (x) => alert(x);
+  const passRef = useRef();
+  const [userName,setUserName] = useState('')
+  const [error, setError] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     // console.log(event.target);     will print html elements form
     // console.dir(event.target);     will print form as objects
-    alertPlease(event.target.elements.username.value)
-    alertPlease(passRef.current.value)
+    alertPlease(event.target.elements.username.value);
+    alertPlease(passRef.current.value);
+  }
+  function handleUserName(event) {
+    const { value } = event.target;
+    let RegEx = /^[a-z0-9]+$/i; 
+    const isAlphanumeric = RegEx.test(value);
+    setError(isAlphanumeric ? null : "Only alphanumric is allowed");
+    setUserName(value.toLowerCase())
+  }
+  function handlePassword(event) {
+    const { value } = event.target;
+    let RegEx = /^[a-z0-9]+$/i; 
+    const isAlphanumeric = RegEx.test(value);
+    setError(isAlphanumeric ? null : "Only alphanumric is allowed");
   }
   return (
     <>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username">
-            <input id="username" type="text"></input>
-          </label>
-          <label>
-            <input ref={passRef} type="text"></input>
-          </label>
-          <button type="submit">
-            Submit
-          </button>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" onChange={handleUserName} value={userName}></input>
         </div>
+        <div>
+          <label>Password</label>
+          <input ref={passRef} type="text" onChange={handlePassword}></input>
+        </div>
+        <div style={{ color: "red" }}>{error}</div>
+        <button type="submit" disabled={Boolean(error)}>
+          Submit
+        </button>
       </form>
     </>
   );
